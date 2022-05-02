e81a1ed34f8b248ea73c7361f21dbdcf0cd564bf
gendosu <gendosu@gmail.com>
2022-05-02 14:14:02 +0900

    docker-compose.ymlをRails仕様に変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # set -eu
 
+yarn
+
+bundle install
+
+# rake db:migrate
+
 if [ -e tmp/pids/*.pid ]; then rm tmp/pids/*.pid; fi
 if [ -e node_modules/.cache/hard-source ]; then rm -rf node_modules/.cache/hard-source; fi
 
